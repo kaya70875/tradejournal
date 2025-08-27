56771450897d9e2c4bb5586c3c6cbb1c670a397d
Ahmet Kaya <shortsforturkey@gmail.com>
2025-08-27 17:42:23 +0300

    Add Navbar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter();
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>

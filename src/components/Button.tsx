15c38c50d2ccb33e415deb3f65bafe4cfaf84651
Ahmet Kaya <shortsforturkey@gmail.com>
2025-08-27 17:43:05 +0300

    feat: create button compoenent

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    name: string;
+    className?: string;
+}
+
+export default function Button({ name, className, ...props }: ButtonProps) {
+    return (
+        <button
+            {...props}
+            type="submit"
+            className={`${className} flex justify-center py-2 px-4 cursor-pointer border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-80`}
+        >
+            {name}
+        </button>
+    )
+}

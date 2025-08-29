fb82f4781fd756e72d4ea772365c210cd16fc232
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-08-29 03:36:33 +0000

    feat: create useProfile hook for getting profile informations

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { supabase } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+type Profile = {
+    id: string;
+    full_name: string;
+}
+
+export const useProfile = (userId: string) => {
+    const [profile, setProfile] = useState<null | Profile>(null);
+    const [loading, setLoading] = useState(false);
+    
+    useEffect(() => {
+        const getProfile = async () => {
+            setLoading(true);
+            const { data: profile, error } = await supabase
+                .from('profiles')
+                .select('*')
+                .eq('id', userId)
+                .single()
+            
+            if(error) {
+                setLoading(false)
+                return console.log('Error while getting profile informations.', error)
+            }
+            setLoading(false);
+            setProfile(profile);
+        }
+
+        getProfile();
+    }, [])
+
+    return {profile, loading};
+}

37c7ad1e6a2988d918d6aa572b36e82592868684
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-08-29 01:25:00 +0000

    fix: supabase fetch error on login actions

--- src/app/(base)/dashboard/page.tsx
+++ src/app/(base)/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default function page() {
+export default async function page() {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.getClaims();
+    if (error || !data?.claims) {
+        redirect("/login");
+    }
     return (
         <div>
 

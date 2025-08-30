6f870632e8ddb094e76256388b648ba7644b3c2b
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-08-30 01:53:19 +0000

    feat: add search input and trade card to dashboard page

--- src/app/(base)/dashboard/page.tsx
+++ src/app/(base)/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import React from 'react'
 import DashboardHeader from '../components/DashboardHeader';
+import SearchInput from '../components/SearchInput';
+import TradeCard from '../components/TradeCard';
 
 export default async function page() {
     const supabase = await createClient();
@@ -11,8 +13,11 @@ export default async function page() {
         redirect("/login");
     }
     return (
-        <div className='ml-[256px] p-8'>
+        <div className='ml-[256px] p-8 flex flex-col gap-4'>
             <DashboardHeader />
+            <SearchInput />
+
+            <TradeCard pair='BTC/USD' reason='Bitcoing showing strong support at $40,000 level. Multiple technical indicators suggesting a bullish reversal pattern.' date='Aug 27, 2025' tags={['long', 'crypto', 'swing']} />
         </div>
     )
 }

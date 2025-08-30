c4067191871594b52b230cde6f94d91099af6d68
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-08-30 01:51:25 +0000

    refactor: remove fixed width from tag component

--- src/app/(base)/components/Tag.tsx
+++ src/app/(base)/components/Tag.tsx
@@ -4,7 +4,7 @@ interface TagProps {
 
 export default function Tag({name} : TagProps) {
     return (
-        <div className="flex items-center justify-center rounded-xl bg-blue-200 text-gray-800 cursor-pointer font-medium text-sm w-12">
+        <div className="flex items-center justify-center rounded-xl bg-blue-200 text-gray-800 cursor-pointer font-medium text-sm px-1 sm:px-2">
             {name}
         </div>
     )

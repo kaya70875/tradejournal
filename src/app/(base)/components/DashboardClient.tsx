e0e1757b37c11857f2e23175ef9270aa1ef037f3
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-01 16:26:49 +0300

    create Dashboard client and stick with server components in dashboard page

---
+++ src/app/(base)/components/DashboardClient.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React, { useState } from 'react'
+import DashboardHeader from './DashboardHeader'
+import SearchInput from './SearchInput'
+import TradeForm from './TradeForm'
+import TradeCard from './TradeCard'
+
+interface TradeCard {
+    pair: string;
+    reason: string;
+    created_at: string;
+    tags: string[];
+}
+
+interface DashboardClientProps {
+    tradeCards: TradeCard[]
+}
+
+export default function DashboardClient({ tradeCards }: DashboardClientProps) {
+
+    const [showTradeForm, setShowTradeForm] = useState(false);
+
+    return (
+        <div className='ml-[256px] p-8 flex flex-col gap-4'>
+            <DashboardHeader onOpen={() => setShowTradeForm(true)} />
+            <SearchInput />
+
+            {showTradeForm && (
+                <TradeForm onClose={() => setShowTradeForm(false)} />
+            )}
+
+            <section className='grid grid-cols-3 gap-4 w-full'>
+                {tradeCards.map((card, idx) => (
+                    <TradeCard pair={card.pair} reason={card.reason} date={card.created_at} tags={card.tags} key={idx} />
+                ))}
+            </section>
+
+
+        </div>
+    )
+}
--- src/app/(base)/dashboard/page.tsx
+++ src/app/(base)/dashboard/page.tsx
@@ -1,9 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import React from 'react'
-import DashboardHeader from '../components/DashboardHeader';
-import SearchInput from '../components/SearchInput';
-import TradeCard from '../components/TradeCard';
+import DashboardClient from '../components/DashboardClient';
 
 export default async function page() {
     const supabase = await createClient();
@@ -12,12 +10,17 @@ export default async function page() {
     if (error || !data?.claims) {
         redirect("/login");
     }
-    return (
-        <div className='ml-[256px] p-8 flex flex-col gap-4'>
-            <DashboardHeader />
-            <SearchInput />
 
-            <TradeCard pair='BTC/USD' reason='Bitcoing showing strong support at $40,000 level. Multiple technical indicators suggesting a bullish reversal pattern.' date='Aug 27, 2025' tags={['long', 'crypto', 'swing']} />
-        </div>
+    /* Fetch cards from supabase */
+
+    const { data: tradeCards, error: errorTradeCards } = await supabase
+        .from('trade')
+        .select('*')
+        .order('created_at', { ascending: false });
+
+    if (errorTradeCards) return console.error(errorTradeCards);
+
+    return (
+        <DashboardClient tradeCards={tradeCards} />
     )
 }

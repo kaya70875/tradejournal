0d29c476e6141f895e196885bc023ec5e27df745
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-02 01:42:23 +0000

    feat: add background overlay for confirmation modal

--- src/app/(base)/components/ConfirmationModal.tsx
+++ src/app/(base)/components/ConfirmationModal.tsx
@@ -9,10 +9,12 @@ interface ConfirmationModalProps {
 
 export default function ConfirmationModal({ onConfirm, onCancel, loading }: ConfirmationModalProps) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="border bg-gray-100 border-gray-200 p-8 flex flex-col gap-6 rounded-lg">
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div className="bg-overlay absolute inset-0 bg-black opacity-50 backdrop-blur-sm" />
+
+            <div className="relative z-10 border border-gray-200 p-8 flex flex-col gap-6 rounded-lg bg-white">
                 <header className='text-gray-900 font-medium'>
-                    Are you sure you want to delete this card ?
+                    Are you sure you want to delete this card?
                 </header>
 
                 <div className='flex items-center gap-4 justify-center w-full'>

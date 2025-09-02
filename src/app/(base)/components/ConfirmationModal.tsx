5d1414d2ababb53340ce93aeeb700b6af330820b
Ahmet Kaya <156709255+kaya70875@users.noreply.github.com>
2025-09-02 03:38:00 +0000

    refactor: update ConfirmationModal component's layout and style

--- src/app/(base)/components/ConfirmationModal.tsx
+++ src/app/(base)/components/ConfirmationModal.tsx
@@ -1,4 +1,5 @@
 import Button from '@/components/Button'
+import { XIcon } from 'lucide-react';
 import React from 'react'
 
 interface ConfirmationModalProps {
@@ -12,14 +13,22 @@ export default function ConfirmationModal({ onConfirm, onCancel, loading }: Conf
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div className="bg-overlay absolute inset-0 bg-black opacity-50 backdrop-blur-sm" />
 
-            <div className="relative z-10 border border-gray-200 p-8 flex flex-col gap-6 rounded-lg bg-white">
-                <header className='text-gray-900 font-medium'>
-                    Are you sure you want to delete this card?
-                </header>
+            <div className="relative z-10 border border-gray-200 w-1/4 flex flex-col rounded-lg bg-white">
+                <section className='flex flex-col items-start'>
+                    <header className='flex items-center w-full justify-between p-3 border-b border-gray-200'>
+                        <p className='text-gray-900 font-medium'>Confirm to delete selected card</p>
+                        <XIcon className='w-4 h-4 text-gray-400 cursor-pointer hover:opacity-80' onClick={onCancel} />
+                    </header>
 
-                <div className='flex items-center gap-4 justify-center w-full'>
-                    <Button onClick={onConfirm} disabled={loading} className='disabled:opacity-80'>{loading ? 'Deleting' : 'Confirm'}</Button>
-                    <Button onClick={onCancel} variant='ghost'>Cancel</Button>
+                    <div className="explanation text-gray-700 text-sm p-3 border-b border-gray-200 w-full">
+                        <p>Are you sure you want to delete this card ?</p>
+                        <p>This action cannot be undone.</p>
+                    </div>
+                </section>
+
+                <div className='flex items-center gap-4 justify-center w-full p-3'>
+                    <Button onClick={onConfirm} disabled={loading} className='disabled:opacity-80 w-full'>{loading ? 'Deleting' : 'Confirm'}</Button>
+                    <Button onClick={onCancel} variant='ghost' className='w-full'>Cancel</Button>
                 </div>
             </div>
         </div>

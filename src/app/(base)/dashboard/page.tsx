edb5ac183ead8c4c4a373745e9eef395d8364546
Ahmet Kaya <shortsforturkey@gmail.com>
2025-09-03 21:55:14 +0300

    fix: validator type error

--- src/app/(base)/dashboard/page.tsx
+++ src/app/(base)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import React from "react";
 import DashboardClient from "../components/DashboardClient";
+import Button from "@/components/Button";
 
 export default async function page() {
   const supabase = await createClient();
@@ -18,7 +19,12 @@ export default async function page() {
     .select("*")
     .order("created_at", { ascending: false });
 
-  if (errorTradeCards) return console.error(errorTradeCards);
+  if (errorTradeCards) return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <h3>An error happened.</h3>
+      <Button onClick={() => redirect('/')}>Try Again</Button>
+    </div>
+  )
 
   return <DashboardClient tradeCards={tradeCards} />;
 }
